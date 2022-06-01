2af75f9a1a0e1862cf64f181ac6c9764e44c0b29
Dennis Gosnell <cdep.illabout@gmail.com>
2022-06-01 23:41:15 +0900

    Bump to fourmolu-0.7.0.1.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const tool_cache = require('@actions/tool-cache');
 const exec = require('@actions/exec');
 const glob = require('@actions/glob');
 
-const fourmolu_version = '0.6.0.0';
+const fourmolu_version = '0.7.0.1';
 
 // XXX: These release binaries appear to be dynamically linked, so they may not
 // run on some systems.
