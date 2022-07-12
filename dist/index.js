89302f3a733d21675a723e3b5075454ac3c33c82
AlexanderHarley <moh_harper@msn.com>
2022-07-12 17:39:48 +0100

    Add missing await to chmod execution

--- dist/index.js
+++ dist/index.js
@@ -76,7 +76,7 @@ async function run() {
 
     // Set mode
 
-    exec.exec('chmod', ['+x', fourmolu_cached_path], {silent: true});
+    await exec.exec('chmod', ['+x', fourmolu_cached_path], {silent: true});
 
     // Glob for the files to format
 
